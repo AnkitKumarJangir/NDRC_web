02a451081fcc22d2db72943585e89a814878f100
= <=>
2025-01-13 19:52:53 +0530

    live url update

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,5 @@
 // export const baseurl = 'https://ndrc-mobile-backend.onrender.com/api';
-export const baseurl = 'http://localhost:3000/api';
+
+// export const baseurl = 'http://localhost:3000/api';
+export const baseurl = 'http://64.227.147.224/api';
+
